2e4e859ea8fc4a813b89fc472648083d08bb0f46
Sieu Tsoi <sieut@umich.edu>
2017-03-25 00:46:28 -0400

    Update domain for new EC2 (using Ubuntu this time)

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # You need the key from me, Sieu, to connect
-ssh -i "kickflip.pem" ec2-user@ec2-52-14-172-176.us-east-2.compute.amazonaws.com << 'ENDSTART'
+ssh -i "kickflip.pem" ubuntu@ec2-52-14-104-27.us-east-2.compute.amazonaws.com << 'ENDSTART'
 cd kickflip/
 git pull
 node main.js
