0d0ac51ae29e1ef53ab0cc42efbf45dd811b2b26
Sieu Tsoi <sieut@umich.edu>
2017-03-25 01:33:56 -0400

    Some minor changes for main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
+	res.send('This is the server for Kickflip');
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+	console.log('Example app listening on port 3000!');
 })
