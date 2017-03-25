c3c2ad1d190660dba15fc11772dca5a71b9944f2
ben wireman <bwireman@umich.edu>
2017-03-25 17:18:35 -0400

    CHANGED some of the output

--- classes.js
+++ classes.js
@@ -162,7 +162,7 @@ class Game {
          var judgeMsg = `The next round is starting! You are the judge. \n\nRespond with a question for the players.`;
 
         for (var i = 0; i < this.players.length; i++) {
-			if judgeIndexi == 0) {
+			if (this.judgeIndex == 0) {
 				if (i == this.judgeIndex) {
 					this.sendText(this.players[i].phoneNumber, judgeMsg);
 				}
@@ -250,15 +250,14 @@ class Game {
 					var judgeName = this.players[this.judgeIndex].name;
 					var winningPlayerIndex = this.answers[choice - 1].playerIndex;
 					this.players[winningPlayerIndex].score += 10;
-					var winnerName = this.players[winningPlayerIndex].name;
 					for (var i = 0; i < this.players.length; ++i) {
-						if (i == this.judgeIndex) {
-							this.sendText(this.players[i].phoneNumber, 'The judge selected ' + winnerName + ' and gave them 10 points\n'
-							`The next round is starting! ${judgeName} is the judge.\n\nWaiting for ${judgeName} to ask a question.`');
+						if (i != this.judgeIndex) {
+						    this.sendText(this.players[i].phoneNumber, 'The judge selected ' + this.answers[choice - 1].text + ' and gave them 10 points\n' + 
+							'`The next round is starting! '+ judgeName + ' `is the judge.\n\nWaiting for '+ judgeName + ' to ask a question.`');
 						}
 						else {
-							this.sendText(this.players[i].phoneNumber, 'The judge selected ' + winnerName + ' and gave them 10 points\n'
-							`The next round is starting! You are the judge. \n\nRespond with a question for the players.`');
+						    this.sendText(this.players[i].phoneNumber, 'The judge selected ' + this.answers[choice - 1].text + ' and gave them 10 points\n' +
+							'`The next round is starting! You are the judge. \n\nRespond with a question for the players.`');
 						}							
 					}
 					this.roundEnd();
@@ -335,8 +334,17 @@ class Game {
 				winnerName = this.players[i].name
 			}
 		}
+
+		var gameScoreboard = "Game over!\nName   Score\n";
+
 		for (var i = 0; i < this.players.length; ++i) {
-			this.sendText(this.players[i].phoneNumber, 'Game over! The winner is ' + winnerName + ' with ' + max + ' points!');
+		    gameScoreboard += this.players[i].name + "   " + this.players[i].score + "\n";
+		}
+		gameScoreboard += 'The winner is ' + winnerName + ' with ' + max + " points!\n"
+		gameScoreboard += winnerName + " is the Kickflip king!"
+
+		for (var i = 0; i < this.players.length; ++i) {
+			this.sendText(this.players[i].phoneNumber, gameScoreboard);
 		}
 		//todo send event emitter to driver function and clear memory and shiz
 	}
