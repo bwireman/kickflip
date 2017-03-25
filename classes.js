3374d516c6552c3391906856c978763396dec4d3
Austin Dudas <dudasaus@umich.edu>
2017-03-25 01:09:05 -0400

    Added helper functions

--- classes.js
+++ classes.js
@@ -1,24 +1,74 @@
 // Game object
  class Game {
-     state = '';
-     name = '';
-     creatorPhoneNumber = '';
-     players = [];
-     judgeIndex = 0;
-     answers = [];
 
      constructor(gameName, phoneNumber, username) {
          this.name = name;
          this.creatorPhoneNumber = phoneNumber;
+
+         // stuff
+         this.state = '';
+         this.players = [];
+         this.judgeIndex = 0;
+         this.answers = [];
+
          this.addPlayer(phoneNumber, username);
      }
 
      addPlayer(phoneNumber, username) {
-         this.players.push(new Player(phoneNumber, username));
-         // if player limit check stuff here
-         // check if number already exists
+         if (this.isValidNumber(phoneNumber) == 0) {
+             this.players.push(new Player(phoneNumber, username));
+         }
+         // todo already added message? maybe??!???!?!?!
      }
- }
+
+     isValidNumber(phoneNumber) {
+         //RETURN INT
+         // 0: not in player array
+         // 1: normal player (not judge)
+         // 2: judge
+         for (i = 0; i < this.players.length; ++i) {
+             if (phoneNumber == this.players[i].phoneNumber) {
+                 if (i == this.judgeIndex) {
+                     return 2;
+                 }
+                 else {
+                     return 1;
+                 }
+             }
+         }
+         return 0;
+     }
+
+     isCreator(phoneNumber) {
+         return phoneNumber == this.creatorPhoneNumber;
+     }
+
+     getPlayer(phoneNumber) {
+         for (i = 0; i < this.players.length; ++i) {
+             if (phoneNumber == this.players[i].phoneNumber) {
+                 return i;
+             }
+         }
+         return -1;
+     }
+     onInput(message, phoneNumber) {
+         // direct program based on its current state
+         if (this.state == "join") {
+
+         }
+         else if (this.state == "judgestart") {
+
+         }
+         else if (this.state == "playerResponses") {
+
+         }
+         else if (this.state == "judging") {
+
+         }
+     }
+
+
+ } //end of game object
 
  /*
 new game text: create GAME_NAME, MY_NAME
