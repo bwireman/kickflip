8f109b477fabd8818883a90079d8b89bc451a790
Sieu Tsoi <sieut@umich.edu>
2017-03-25 23:13:37 -0400

    USE SELF NOT THIS GOD DAMN IT - BY SIEU

--- classes.js
+++ classes.js
@@ -371,8 +371,8 @@ class Game {
                     var self = this;
 			        this.pgDriver.getRandomQuestion(function(question) {
                         self.question = question;
-                        this.sendText(phoneNumber, 'You sent: ' + question + '\n Now waiting for player responses');
-                        this.judgeStartToPlayerResponse(); //advance state
+                        self.sendText(phoneNumber, 'You sent: ' + question + '\n Now waiting for player responses');
+                        self.judgeStartToPlayerResponse(); //advance state
 
                     });
 			        console.log("asked for a random question, advance to state player response")
