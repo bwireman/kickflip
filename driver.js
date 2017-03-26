4acf306bd2909ac634f9636df7cf871e8738547d
nick schaible <schainic@umich.edu>
2017-03-25 20:59:07 -0400

    did a thing

--- driver.js
+++ driver.js
@@ -38,7 +38,7 @@ class Driver {
 
 		this.game = new Classes.Game(args[0], senderNumber, args[1], false);
 		this.game.sendText(senderNumber, "Welcome to Kickflip, " + args[1] + " , Game: " + args[0] + " has been created!\n\n Have Fun!\n\n" +
-						   "Invite your friends by texting \"invite ...\" follwed by their phone numbers separated by commas, or tell them to text \" "+ args[0] + ", \"name\" to  " +
+						   "Invite your friends by texting \"invite ...\" follwed by their phone numbers separated by commas, or tell them to text \" "+ args[0] + ", theirName\" to  " +
 						   twilioNumber + ". Text \"start\" to begin your game!");
 	}
 
