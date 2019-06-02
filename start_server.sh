ee637e72ae93ab90351fa43db15bd09a4cbebe37
bwireman <bwireman1@gmail.com>
2019-06-02 19:20:19 -0400

    start server

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # You need the key from me, Ben, to connect
-ssh -i "kickflip.pem" ubuntu@ec2-13-59-33-94.us-east-2.compute.amazonaws.com << 'ENDSTART'
+ssh -i "~/.ssh/kickflip.pem" ubuntu@ec2-13-59-33-94.us-east-2.compute.amazonaws.com << 'ENDSTART'
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
 cd kickflip/
 git pull
